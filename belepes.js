68d9ed5179ea43729bf810fc802bd33993e4a341
ElitAlakulat <63428119+ElitAlakulat@users.noreply.github.com>
2020-04-11 18:13:33 +0200

    Update belepes.js

--- belepes.js
+++ belepes.js
@@ -1,6 +1,12 @@
+var fs = require('fs');
 function cehtag(){
+	fs.readFile('jelszavak.txt', 'utf8',function(err, data){
+		alert(data);
+	});
+	alert(szoveg);
+	/*var text=fs.readFile("jelszavak.txt");
 	adat=prompt("Jelszó:");
 	if(adat=="alma123"){
-		window.location = "docs/index.html";
-	}
+		window.location = "index.html";
+	}*/
 }
