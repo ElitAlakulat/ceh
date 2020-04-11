6ae9cfd82ebd1d82784b9703630951a162246ce8
ElitAlakulat <63428119+ElitAlakulat@users.noreply.github.com>
2020-04-11 17:44:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+﻿<!DOCTYPE html>
+<html lang="hu">
+	<head>
+		<meta charset="utf-8">
+		<script src="belepes.js"></script>
+		<style>
+			#udv{
+				position:relative;
+				top:50%;
+				transform:translate(-50%, -50%);
+				left:50%;
+				display:block;
+				width:950px;
+				left:50%
+			}
+			h1{
+				text-align:center;
+			}
+			button{
+				display:block;
+				margin:10px auto;
+			}
+			button:nth-child(2){
+				
+			}
+		</style>
+		<script>
+			
+		</script>
+	</head>
+	<body >
+		<div id="udv">
+			<h1>Üdvözöllek az Elit Alakulat honlapján!</h1>
+			<button onclick="cehtag()">Céhtag vagyok</button>
+			<button onclick="window.location = index.html">Látogató vagyok</button>
+		</div>
+		<?php
+echo "My first PHP script!";
+?>
+		
+	</body>
+</html>
